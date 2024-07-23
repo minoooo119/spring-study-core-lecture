a7f57f1c3d46a9a8b0ad545c485516dea12f5744
minoooo119 <minoth0119@naver.com>
2024-07-23 11:16:30 +0900

    [fix] 주석 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 public class MemberServiceImpl implements MemberService {
 
+    //현재는 추상화 구체화 모두 의존하고 있음 DIP 위배하고 있다.
     private final MemoryRepository memoryRepository = new MemoryMemberRepository();
 
     @Override
