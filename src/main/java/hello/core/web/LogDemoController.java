cfb61e76c6f95c4e59dd427c6d270389260f0c38
minoooo119 <minoth0119@naver.com>
2024-08-01 10:19:57 +0900

    [add] web scope - 오류 수정 by Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,8 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +14,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger; //얘는 request 호출 시에 생성되어서 의존 관계가 주입 되지 않아서 오류가 난다. 수정이 필요
+    //    private final MyLogger myLogger; //얘는 request 호출 시에 생성되어서 의존 관계가 주입 되지 않아서 오류가 난다. 수정이 필요
+    //즉, 요청 시점에 Provider 로 주입해줘야한다.
+    private final ObjectProvider<MyLogger> objectProvider;
 
+
+    /**
+     * clientA
+     * [47ecea06-a3f5-4105-b844-c822ea6f4d11] request scope bean create:hello.core.common.MyLogger@20e0e1c8
+     * [47ecea06-a3f5-4105-b844-c822ea6f4d11][/log-demo] controller test
+     * [47ecea06-a3f5-4105-b844-c822ea6f4d11][/log-demo] service id = testID
+     * [47ecea06-a3f5-4105-b844-c822ea6f4d11] request scope bean close:hello.core.common.MyLogger@20e0e1c8
+     * clientB
+     * [295f35d7-0ab3-45c9-b9fd-d604215418b8] request scope bean create:hello.core.common.MyLogger@6e7a227a
+     * [295f35d7-0ab3-45c9-b9fd-d604215418b8][/log-demo] controller testr
+     * [295f35d7-0ab3-45c9-b9fd-d604215418b8][/log-demo] service id = testID
+     * [295f35d7-0ab3-45c9-b9fd-d604215418b8] request scope bean close:hello.core.common.MyLogger@6e7a227a
+     */
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request){
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURI().toString();
+        System.out.println("requestURL = " + requestURL);
+
+        MyLogger myLogger = objectProvider.getObject();
+
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testID");
 
         return "OK";
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,14 +2,26 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+//    private final MyLogger myLogger; //얘는 request 호출 시에 생성되어서 의존 관계가 주입 되지 않아서 오류가 난다. 수정이 필요
+    //즉, 요청 시점에 Provider 로 주입해줘야한다.
+    private final ObjectProvider<MyLogger> objectProvider;
+
+    /**
+     * [2389c405-f87e-4d7c-ae08-c14316eebe82] request scope bean create:hello.core.common.MyLogger@2d1196a7
+     * [2389c405-f87e-4d7c-ae08-c14316eebe82][/log-demo] controller test
+     * [2389c405-f87e-4d7c-ae08-c14316eebe82][/log-demo] service id = testID
+     * [2389c405-f87e-4d7c-ae08-c14316eebe82] request scope bean close:hello.core.common.MyLogger@2d1196a7
+     */
     public void logic(String id) {
+        MyLogger myLogger = objectProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
